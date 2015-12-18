1c371d39654026e298f5977a78119c0a1cff6267
Nizar SEHLI <nisehl@gmail.com>
2015-12-18 12:10:44 +0100

    Initial Messages implementation

---
+++ src/main/scala/Messages.scala
@@ -0,0 +1,9 @@
+package com.zengularity.raftology
+
+sealed trait Entry
+case class Heartbeat extends Entry
+case class AppendEntries(entry: Any) extends Entry // For sqlite, entry is going to be sql command
+
+case object RequestVote
+case object AcceptVote
+case object DenyVote
